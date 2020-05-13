805f38d0f981f6b4d300e64dfd8f43b9c5f9d615
Jhoane-Black <54192804+Jhoane-Black@users.noreply.github.com>
2020-05-12 22:14:37 -0500

    Create 2.php

---
+++ bookstore/2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            <title>Bookstore</title>
+    </head>
+    <body>
+        <?php
+            include('C:\xampp\htdocs\bookstore\controller\connection.php');
+            $sql="select * from book ORDER BY book_id ASC LIMIT 5, 5;";
+            $resultado=mysqli_query($con,$sql);
+        ?>
+       <div>
+            <a href="model\create.php">Nuevo Libro</a><br>
+            <form action="model\read.php"> 
+                <input type="text" name="read" id="read" placeholder="Buscar">
+                <input type="submit" value="Buscar"  ><br>                        
+            </form>
+          <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>title</th>
+                        <th>editorial</th>
+                        <th>saga</th>
+                        <th>price</th>
+                    </tr>
+                <thead>
+                <tbody>
+                    <?php while ($rows=mysqli_fetch_assoc($resultado)) {                           
+                    ?>
+                    <tr>
+                        <td><?php echo $rows['book_id']?></td>
+                        <td><?php echo $rows['book_title']?></td>
+                        <td><?php echo $rows['book_editorial']?></td>
+                        <td><?php echo $rows['book_saga']?></td>
+                        <td><?php echo $rows['book_price']?></td>
+                        <td>
+                            <a href="model\update.php?id=<?php echo $rows['book_id']?>">Editar</a>
+                            <a href="model\delete.php?id=<?php echo $rows['book_id']?>">Eliminar</a>
+                        </td>
+                    </tr>
+                    <?php } ?>                    
+                </tbody>
+            <table>
+        </div>
+    </body>
+</html> 
+<form> 
+&nbsp
+<a href="index.php"><< Anterior</a>&nbsp
+<a href="index.php">1</a>&nbsp
+<a href="2.php">2</a>&nbsp
+<a href="3.php">3</a>&nbsp
+<a href="3.php">Siguiente >></a>   
+</form>
